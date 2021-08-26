3328b2a0b8e05b12328099570174e79f3188d3b1
laurazhi <you@example.com>
2021-08-25 19:45:44 -0700

    Initial commit

---
+++ pageObjects/LoginPage.py
@@ -0,0 +1,22 @@
+class LoginPage():
+    button_homeUser_xpath="//a[@href='/users/auth/dexcom_sts']"
+    textbox_username_name="username"
+    textbox_password_name="password"
+    button_login_xpath="//input[@type='submit']"
+
+    def __int__(self,driver):
+        self.driver=driver
+
+    def clickHomeUser(self):
+        self.driver.find_element_by_xpath(self.button_homeUser_xpath).click()
+
+    def setUserName(self,username):
+        self.driver.find_element_by_name(self.textbox_username_name).send_keys(username)
+
+    def setPassword(self,password):
+        self.driver.find_element_by_name(self.textbox_password_name).send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element_by_xpath(self.button_login_xpath).click()
+
+
---
+++ testCases/LoginTest.py
@@ -0,0 +1,40 @@
+import unittest
+from datetime import time
+import HTMLTestRunner
+from selenium import webdriver
+import sys
+sys.path.append("C://Users/laura/PycharmProjects/pythonProject/pythonProject1")
+from pageObjects.LoginPage import LoginPage
+
+class LoginTest(unittest.TestCase):
+    username = "codechallenge"
+    password = "Password123"
+    baseURL = "https://clarity.dexcom.com"
+    driver = webdriver.Chrome(
+        executable_path="C:\\Users\\laura\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe")
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver.get(cls.baseURL)
+        cls.driver.maximize_window()
+
+    def test_login(self):
+        lp=LoginPage(self.driver)
+        lp.clickHomeUser()
+        lp.setUserName(self.username)
+        lp.textbox_password_name(self.password)
+        lp.clickLogin()
+        time.sleep(5)
+        self.assertEqual("Dexcom CLARITY", self.driver.title, "webpage title is not matching")
+
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__=='__main__':
+    unittest.main(testRunner=HTMLTestRunner.HTMLTestRunner(out='..\\reports'))
+
+
+
+
+
+
